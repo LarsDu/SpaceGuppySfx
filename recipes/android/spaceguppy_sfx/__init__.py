7afb47931988e647050ecc257d9d9564d7d9d976
Lawrence Du <larrydu88@gmail.com>
2018-05-23 22:30:35 -0700

    Fixed android recipe

--- recipes/android/spaceguppy_sfx/__init__.py
+++ recipes/android/spaceguppy_sfx/__init__.py
@@ -1,6 +1,15 @@
-from toolchain import CythonRecipe, #shprint
+from pythonforandroid.recipe import CythonRecipe
 from os.path import join
 #import sh
+
+
+"""
+Note for future recipe creation.
+
+The best way to structure a project is to push add-on modules to github, 
+and place setup.py in the very top directory. Setup.py should have build 
+instructions for different architectures.
+"""
 
 class SpaceGuppySfxRecipe(CythonRecipe):
     version = 'master'
@@ -9,8 +18,24 @@ class SpaceGuppySfxRecipe(CythonRecipe):
     depends=['kivent_core']
     cythonize= True
 
-    def prepare_build_dir(self, arch):
-        '''No need to prepare '''
-        return
+
+
+
+    def get_recipe_env(self, arch, with_flags_in_cc=True):
+        env = super(SpaceGuppySfxRecipe, self).get_recipe_env(
+            arch, with_flags_in_cc=with_flags_in_cc)
+        cymunk = self.get_recipe('cymunk', self.ctx).get_build_dir(arch.arch)
+        env['PYTHONPATH'] = join(cymunk, 'cymunk', 'python')
+        kivy = self.get_recipe('kivy', self.ctx).get_build_dir(arch.arch)
+        kivent = self.get_recipe('kivent_core',
+                                 self.ctx).get_build_dir(arch.arch, sub=True)
+        env['CYTHONPATH'] = ':'.join((kivy, kivent))
+
+
+        return env
+    
+
+
+
 
 recipe = SpaceGuppySfxRecipe()
