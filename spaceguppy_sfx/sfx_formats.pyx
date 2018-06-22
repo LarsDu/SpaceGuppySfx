3b17b8966ef7fa934c0b7de49b492b8957b02a7a
Lawrence Du <larrydu88@gmail.com>
2018-06-22 00:15:44 -0700

    Added v_tint offset

--- spaceguppy_sfx/sfx_formats.pyx
+++ spaceguppy_sfx/sfx_formats.pyx
@@ -11,12 +11,14 @@ rot_offset = <Py_ssize_t> (<Py_intptr_t>(&tmp_var1.rotate) - <Py_intptr_t>(tmp_v
 scale_offset = <Py_ssize_t> (<Py_intptr_t>(tmp_var1.scale) - <Py_intptr_t>(tmp_var1))
 render_rot_offset = <Py_ssize_t> (<Py_intptr_t>(&tmp_var1.render_rotate) - <Py_intptr_t>(tmp_var1))
 color_offset = <Py_ssize_t> (<Py_intptr_t>(tmp_var1.v_color) - <Py_intptr_t>(tmp_var1))
+tint_offset = <Py_ssize_t> (<Py_intptr_t>(tmp_var1.v_tint) - <Py_intptr_t>(tmp_var1))
 x_trans_offset = <Py_ssize_t> (<Py_intptr_t>(&tmp_var1.x_trans) - <Py_intptr_t>(tmp_var1))#
 y_trans_offset = <Py_ssize_t> (<Py_intptr_t>(&tmp_var1.y_trans) - <Py_intptr_t>(tmp_var1))#
 x_shear_offset = <Py_ssize_t> (<Py_intptr_t>(&tmp_var1.x_shear) - <Py_intptr_t>(tmp_var1))#
 y_shear_offset = <Py_ssize_t> (<Py_intptr_t>(&tmp_var1.y_shear) - <Py_intptr_t>(tmp_var1))#
 ee_color_offset = <Py_ssize_t> (<Py_intptr_t>(tmp_var1.edge_effect_color) - <Py_intptr_t>(tmp_var1))
 ee_thresh_offset = <Py_ssize_t> (<Py_intptr_t>(&tmp_var1.edge_effect_thresh) - <Py_intptr_t>(tmp_var1))
+
 #sfx_flag_offset = <Py_ssize_t> (<Py_intptr_t>(&tmp_var1.sfx_flag) - <Py_intptr_t>(tmp_var1))
 
 vertex_format_sfx1 = [
@@ -27,6 +29,7 @@ vertex_format_sfx1 = [
     (b'rotate', 1, b'float', rot_offset,False),
     (b'render_rotate', 1, b'float', render_rot_offset,False),
     (b'v_color', 4, b'ubyte', color_offset,True),
+    (b'v_tint', 4, b'float, tint_offset, True),
     (b'x_trans', 1, b'float', x_trans_offset,False),
     (b'y_trans', 1, b'float',  y_trans_offset,False),
     (b'x_shear', 1, b'float', x_shear_offset,False),
